85ab6b52058d9af7b0474cba1b8b9a87ac568747
Victor1890 <victorrosariodeveloper@gmail.com>
2020-09-13 09:36:43 -0400

    [ADD] queue and skip for the music

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const ytdl = require("ytdl-core");
 const TOKEN = process.env.DISCORD_TOKEN;
 const PREFIX = `!`;
 
+const queue = new Map();
+
 const client = new Client();
 // const msgEmbed = require("./utils/MsgEmbed");
 // const rules = require("./utils/rules.json");
@@ -17,41 +19,89 @@ client.on("message", async (message) => {
   if (!message.content.startsWith(PREFIX)) return;
 
   const args = message.content.substring(PREFIX.length).split(" ");
+  const serverQueue = queue.get(message.guild.id);
 
   if (message.content.startsWith(`${PREFIX}play`)) {
     const voiceChannel = message.member.voice.channel;
+
     if (!voiceChannel)
       return message.channel.send("You need to be in a voice channel to play");
+
     const permission = voiceChannel.permissionsFor(message.client.user);
+
     if (!permission.has("CONNECT"))
       return message.channel.send(
         `I don't have permissions to connect to the voice channel`
       );
+
     if (!permission.has("SPEAK"))
       return message.channel.send(
         `I don't have permissions to 'Speak' in the voice channel`
       );
 
-    try {
-      var connection = await voiceChannel.join();
-    } catch (error) {
-      return message.channel.send(
-        "There was an error connecting to the voice channel"
-      );
+    const songInfo = await ytdl.getInfo(args[1]);
+    const song = {
+      title: songInfo.title,
+      url: songInfo.video_url,
+    };
+
+    if (!serverQueue) {
+      const queueConstruct = {
+        textChannel: message.channel,
+        voiceChannel: voiceChannel,
+        connection: null,
+        songs: [],
+        volume: 5,
+        playing: true,
+      };
+      queue.set(message.guild.id, queueConstruct);
+      queueConstruct.songs.push(song);
+
+      try {
+        var connection = await voiceChannel.join();
+
+        queueConstruct.connection = connection;
+
+        play(message.guild, queueConstruct.songs[0]);
+      } catch (error) {
+        queue.delete(message.guild.id);
+
+        return message.channel.send(
+          "There was an error connecting to the voice channel"
+        );
+      }
+    } else {
+      serverQueue.songs.push(song);
+      return message.channel.send(`**${song.title}** has been added the queue`);
     }
 
-    const dispatcher = connection.play(ytdl(args[1])).on("finish", () => {
-      voiceChannel.leave();
-    });
-    dispatcher.setVolumeLogarithmic(5 / 5);
+    return;
+
+    //
   } else if (message.content.startsWith(`${PREFIX}stop`)) {
     if (!message.member.voice.channel)
       return message.channel.send(
         "You need to be in a voice channel to stop music"
       );
 
-    message.member.voice.channel.leave();
+    if (!serverQueue) return message.channel.send("There is nothing playing");
+
+    serverQueue.songs = [];
+
+    serverQueue.connection.dispatcher.end();
+
+    message.channel.send("I have stoped the music for you");
+
     return undefined;
+
+    //
+  } else if (message.content.startsWith(`${PREFIX}skip`)) {
+    if (!message.member.voice.channel)
+      return message.channel.send(
+        "You need to be in voice channel to skip the music"
+      );
+
+    if (!serverQueue) return message.channel.send("There nothing playing");
   }
   // if (message.content.toLocaleLowerCase() === "!rules") {
   //   message.channel.send(
@@ -63,6 +113,25 @@ client.on("message", async (message) => {
   //   );
   // }
 });
+
+const play = (guild, song) => {
+  const serverQueue = queue.get(guild.id);
+
+  if (!song) {
+    serverQueue.voiceChannel.leave();
+    queue.delete(guild.id);
+    return;
+  }
+
+  const dispatcher = serverQueue.connection
+    .play(ytdl(song.url))
+    .on("finish", () => {
+      serverQueue.songs.shift();
+      play(guild, serverQueue.songs[0]);
+    });
+
+  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+};
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
